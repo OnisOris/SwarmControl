f95b6ffe2da96d741a881c45b42bd80705a1d37d
Michael <onisoris@yandex.ru>
2024-05-30 15:25:06 +0300

    Добавлены функции для поворотов дронов и массива из дронов

--- swarmsys.py
+++ swarmsys.py
@@ -1,9 +1,37 @@
 import numpy as np
 
 
+def rot_x(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
+
+    rotate_x = np.array([[1, 0, 0],
+                         [0, np.cos(angle), -np.sin(angle)],
+                         [0, np.sin(angle), np.cos(angle)]])
+    rot_vector = rotate_x.dot(vector)
+    return rot_vector
+
+
+def rot_y(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
+    rotate_y = np.array([[np.cos(angle), 0, np.sin(angle)],
+                         [0, 1, 0],
+                         [-np.sin(angle), 0, np.cos(angle)]])
+    rot_vector = rotate_y.dot(vector)
+    return rot_vector
+
+
+def rot_z(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
+    rotate_z = np.array([[np.cos(angle), -np.sin(angle), 0],
+                         [np.sin(angle), np.cos(angle), 0],
+                         [0, 0, 1]])
+    rot_vector = rotate_z.dot(vector)
+    return rot_vector
+
 class Drone:
-    def __init__(self, point: list = [0, 0, 0] | np.ndarray[0, 0, 0],
-                 orientation: list = [0, 0, 0] | np.ndarray[0, 0, 0]):
+    """
+    Класс единицы дрона в трехмерном пространстве.
+    """
+
+    def __init__(self, point: list[0, 0, 0] | np.ndarray,
+                 orientation: list[1, 0, 0] | np.ndarray):
         """
         В данной реализации дрон имеет координату point и вектор ориантации orintation в глобальной системе координат
         :param point:
@@ -14,15 +42,60 @@ class Drone:
 
     def goto(self, point: None = None | list | np.ndarray, orientation: None = None | list | np.ndarray):
         """
-        Функция перемещает дрон в заданное положение
-        :param point:
+        Функция перемещает дрон в заданное положение. Если задана точка назначения и вектор ориентации, то дрон поменяет
+        то и то. Задана только ориентация или только точка, то изменится только нужный параметр. Если не задано ничего,
+        то ничего не поменяется.
+        :param orientation:
+        :type orientation: list or np.ndarray or None
+        :param point: Точка назначения для дрона
+        :type point: list[float, int] or None
         :return:
         """
-
+        if point is None and orientation is None:
+            return
         # Здесь будет код для перемещения дрона, например через piosdk
 
         self.orientation = orientation
         self.point = point
+
+    def apply_position(self) -> None:
+        """
+        Данная функция отправляет дронам изменившеюся ориентацию и позицию в orientation и в point
+        :return: None
+        """
+        # Здесь будет код для перемещения дрона, например через piosdk
+        pass
+
+    def euler_rotate(self, alpha: float, beta: float, gamma: float, apply: bool = False) -> None:
+        self.rot_z(alpha)
+        self.rot_x(beta)
+        self.rot_z(gamma)
+        if apply:
+            self.apply_position()
+
+    def rot_x(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_x(self.orientation, angle)
+        if apply:
+            self.apply_position()
+
+    def rot_y(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_y(self.orientation, angle)
+        if apply:
+            self.apply_position()
+
+    def rot_z(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_z(self.orientation, angle)
+        if apply:
+            self.apply_position()
+
+    def show(self, ax):
+        ax.quiver(self.point[0],
+                  self.point[1],
+                  self.point[2],
+                  self.orientation[0],
+                  self.orientation[1],
+                  self.orientation[2],
+                  color='r')
 
 
 class Darray:
@@ -31,16 +104,25 @@ class Darray:
     """
 
     def __init__(self, drones: list[Drone] | np.ndarray[Drone],
-                 xyz=list[0, 0, 0] | np.ndarray[0, 0, 0],
-                 orientation=np.ndarray[1, 0, 0]):
+                 xyz: list[0, 0, 0] | np.ndarray,
+                 orientation: list[0, 0, 0] | np.ndarray):
         self.drones = drones
-        self.xyz = xyz  # Координата нуля локальной системы координат
+        self.xyz = xyz  # Координата дрона во внешней системе координат
         self.orientation = orientation  # Вектор ориентации системы коорднат массива дронов
 
     def __getitem__(self, item):
         return self.drones[item]
 
-    def create_square_array(self, count: int) -> None:
+    def create_square_array(self, length: float, width: float) -> None:
+        """
+        Функция генерирует квадрат из дронов
+        :param length: Длина квадрата
+        :type length: float
+        :param width: Ширина квадрата
+        :type width: float
+        :return:
+        """
+
         pass
 
     def fasten(self) -> None:
@@ -57,19 +139,32 @@ class Darray:
         """
         pass
 
-    def rotX(self, angle: float | int) -> None:
-        rotateX = np.array([[1, 0, 0],
-                            [0, np.cos(angle), -np.sin(angle)],
-                            [0, np.sin(angle), np.cos(angle)]])
-        self.orientation = rotateX.dot(self.orientation)
+    def apply_position(self) -> None:
+        """
+        Данная функция отправляет дронам изменившеюся ориентацию и позицию в orientation и в point
+        :return: None
+        """
+        # Здесь будет код для перемещения дрона, например через piosdk
+        pass
 
-    def rotY(self, angle: float | int) -> None:
-        rotateY = np.array([[np.cos(angle), 0, np.sin(angle)],
-                            [0, 1, 0],
-                            [-np.sin(angle), 0, np.cos(angle)]])
-        self.orientation = rotateY.dot(self.orientation)
-    def rotZ(self, angle: float | int) -> None:
-        rotateZ = np.array([[np.cos(angle), -np.sin(angle), 0],
-                            [np.sin(angle), np.cos(angle), 0],
-                            [0, 0, 1]])
-        self.orientation = rotateZ.dot(self.orientation)
+    def euler_rotate(self, alpha: float, beta: float, gamma: float, apply: bool = False) -> None:
+        self.rot_z(alpha)
+        self.rot_x(beta)
+        self.rot_z(gamma)
+        if apply:
+            self.apply_position()
+
+    def rot_x(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_x(self.orientation, angle)
+        if apply:
+            self.apply_position()
+
+    def rot_y(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_y(self.orientation, angle)
+        if apply:
+            self.apply_position()
+
+    def rot_z(self, angle: float | int, apply: bool = False) -> None:
+        self.orientation = rot_z(self.orientation, angle)
+        if apply:
+            self.apply_position()
