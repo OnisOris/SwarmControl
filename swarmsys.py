c29946bddc0f12f5d9e91299d1be907ff42826ec
Michael <onisoris@yandex.ru>
2024-06-05 17:47:52 +0300

    Функция отображения и создание массива дронов Функция вращения вокруг произвольной оси Испарвлены баги

--- swarmsys.py
+++ swarmsys.py
@@ -1,8 +1,11 @@
 import numpy as np
+from numpy import cos, sin, pi
 from body import Body
+from dspl import Dspl
+from loguru import logger
+
 
 def rot_x(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
-
     rotate_x = np.array([[1, 0, 0],
                          [0, np.cos(angle), -np.sin(angle)],
                          [0, np.sin(angle), np.cos(angle)]])
@@ -19,11 +22,31 @@ def rot_y(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
 
 
 def rot_z(vector: list | np.ndarray, angle: float | int) -> np.ndarray:
-    rotate_z = np.array([[np.cos(angle), -np.sin(angle), 0],
-                         [np.sin(angle), np.cos(angle), 0],
+    rotate_z = np.array([[cos(angle), -sin(angle), 0],
+                         [sin(angle), cos(angle), 0],
                          [0, 0, 1]])
     rot_vector = rotate_z.dot(vector)
     return rot_vector
+
+
+def rot_v(axis: list | np.ndarray, vector: list | np.ndarray, angle: float | int) -> np.ndarray:
+    """
+    Функция вращает входные вектора вокруг произвольной оси, заданной векторами-столбцами
+    :param axis: Вращаемые вектора
+    :param vector:
+    :param angle:
+    :return:
+    """
+    x, y, z = axis
+    rotate = np.array([[cos(angle) + (1 - cos(angle)) * x**2, (1-cos(angle) * x * y) -
+                        sin(angle)*z, (1 - cos(angle)) * x * z + sin(angle) * y],
+                       [(1 - cos(angle)) * y * x + sin(angle)*z, cos(angle) +
+                        (1 - cos(angle)) * y**2, (1 - cos(angle)) * y * z - sin(angle) * x],
+                       [(1 - cos(angle)) * z * x - sin(angle) * y, (1 - cos(angle)) * z * y +
+                        sin(angle) * x, cos(angle) + (1 - cos(angle)) * z**2]])
+    rot_vector = rotate.dot(vector)
+    return rot_vector
+
 
 class Drone:
     """
@@ -32,9 +55,8 @@ class Drone:
 
     def __init__(self, point: np.ndarray = np.array([0, 0, 0]),
                  orientation: np.ndarray = np.array([[1, 0, 0],
-                                                    [0, 1, 0],
-                                                    [0, 0, 1]]),
-                 ax=None):
+                                                     [0, 1, 0],
+                                                     [0, 0, 1]])):
         """
         В данной реализации дрон имеет координату point и вектор ориантации orintation в глобальной системе координат
         :param point:
@@ -42,10 +64,7 @@ class Drone:
         """
         self.point = point
         self.orientation = orientation
-        self.ax = ax
-        self.body = Body(self.point, self.orientation, ax=self.ax)
-
-
+        self.body = Body(self.point, self.orientation)
 
     def attach_body(self, body):
         self.body = body
@@ -74,7 +93,8 @@ class Drone:
         :return: None
         """
         # Здесь будет код для перемещения дрона, например через piosdk
-        pass
+        self.body.orientation = self.orientation
+        self.body.point = self.point
 
     def euler_rotate(self, alpha: float, beta: float, gamma: float, apply: bool = False) -> None:
         self.rot_z(alpha)
@@ -84,22 +104,36 @@ class Drone:
             self.apply_position()
 
     def rot_x(self, angle: float | int, apply: bool = False) -> None:
-        self.orientation = rot_x(self.orientation, angle)
+        self.orientation = rot_x(self.orientation, -angle)
+        self.point = rot_x(self.point, angle)
         if apply:
             self.apply_position()
 
     def rot_y(self, angle: float | int, apply: bool = False) -> None:
-        self.orientation = rot_y(self.orientation, angle)
+        self.orientation = rot_y(self.orientation, -angle)
+        self.point = rot_y(self.point, angle)
         if apply:
             self.apply_position()
 
-    def rot_z(self, angle: float | int, apply: bool = False) -> None:
-        self.orientation = rot_z(self.orientation, angle)
+    def rot_z(self, angle: float | int, rot_point: np.ndarray = np.array([1, 0, 0]), apply: bool = False) -> None:
+        self.orientation = rot_z(self.orientation, -angle)
+        self.point = rot_z(self.point - rot_point, angle) + rot_z(self.point, angle)
         if apply:
             self.apply_position()
+    # def rot_z(self, angle: float | int, rot_point: np.ndarray = np.array([1, 0, 0]), apply: bool = False) -> None:
+    #     self.orientation = rot_z(self.orientation, angle)
+    #     for drone in self.drones:
+    #         drone.orientation = rot_z(drone.orientation - rot_point, angle) + rot_point
+    #     if apply:
+    #         self.apply_position()
 
-    def show(self):
-        self.body.show()
+    def self_show(self):
+        dp = Dspl([self.body], create_subplot=True, qt=True)
+        self.body.show(dp.ax)
+        dp.show()
+
+    def show(self, ax):
+        self.body.show(ax)
 
 
 class Darray:
@@ -107,17 +141,20 @@ class Darray:
     Данный класс хранит в себе массив с дронами и имеет методы для их общего управления
     """
 
-    def __init__(self, drones: list[Drone] | np.ndarray[Drone],
-                 xyz: list[0, 0, 0] | np.ndarray,
-                 orientation: list[0, 0, 0] | np.ndarray):
+    def __init__(self, drones: list[Drone] | np.ndarray[Drone] = None,
+                 xyz: list | np.ndarray = np.array([0, 0, 0]),
+                 orientation: np.ndarray = np.array([[1, 0, 0],
+                                                     [0, 1, 0],
+                                                     [0, 0, 1]])):
         self.drones = drones
-        self.xyz = xyz  # Координата дрона во внешней системе координат
+        self.xyz = xyz  # Координата массива дронов во внешней системе координат
         self.orientation = orientation  # Вектор ориентации системы коорднат массива дронов
 
     def __getitem__(self, item):
         return self.drones[item]
 
-    def create_square_array(self, length: float, width: float) -> None:
+    def create_square_array(self, sizes: list | np.ndarray = np.array([[-10, 10], [-10, 10]]),
+                            number_of_drones: int = 10) -> None:
         """
         Функция генерирует квадрат из дронов
         :param length: Длина квадрата
@@ -127,7 +164,18 @@ class Darray:
         :return:
         """
 
-        pass
+        x = np.linspace(sizes[0, 0], sizes[0, 1], number_of_drones)
+        y = np.linspace(sizes[1, 0], sizes[1, 1], number_of_drones)
+        z = np.ones(number_of_drones ** 2)
+        x_m, y_m = np.meshgrid(x, y)
+        x_m = x_m.reshape(-1)
+        y_m = y_m.reshape(-1)
+        points = np.vstack((x_m, y_m, z)).T
+        # logger.debug(points)
+        drones = np.array([])
+        for point in points:
+            drones = np.hstack((drones, Drone(point, self.orientation)))
+        self.drones = drones
 
     def fasten(self) -> None:
         """
@@ -168,7 +216,19 @@ class Darray:
         if apply:
             self.apply_position()
 
-    def rot_z(self, angle: float | int, apply: bool = False) -> None:
+    def rot_z(self, angle: float | int, rot_point: np.ndarray = np.array([1, 0, 0]), apply: bool = False) -> None:
         self.orientation = rot_z(self.orientation, angle)
+        for drone in self.drones:
+            drone.orientation = rot_z(drone.orientation - rot_point, angle) + rot_point
         if apply:
             self.apply_position()
+
+    def show(self, ax):
+        for drone in self.drones:
+            drone.show(ax)
+
+    def self_show(self):
+        dp = Dspl(self.drones, create_subplot=True, qt=True)
+        for drone in self.drones:
+            drone.show(dp.ax)
+        dp.show()
