289cbd36fb596d4c510c3597c6924ad4674be8e9
Michael <onisoris@yandex.ru>
2024-06-05 17:46:20 +0300

    подписи к точкам

--- body.py
+++ body.py
@@ -6,40 +6,43 @@ class Body:
                  orientation: np.ndarray = np.array([[1, 0, 0],
                                                     [0, 1, 0],
                                                     [0, 0, 1]]),
-                 ax=None,
                  k=1):
-        self.drone = None
-        # self.trajectory = None
-        self.ax = ax
+        self.name_body = None
         # Текущая координата тела
-        self.point = np.array([0, 0, 0])
+        self.point = point
         # Матрица ориентации
-        self.orientation = np.array([[1, 0, 0],
-                                     [0, 1, 0],
-                                     [0, 0, 1]])
+        self.orientation = orientation
         self.length = 1
         self.k = k
         self.lim_x = np.array([-1, 1])*k
         self.lim_y = np.array([-1, 1])*k
         self.lim_z = np.array([-1, 1])*k
+        self.text = False
 
 
-    def show(self) -> None:
+    def show(self, ax) -> None:
         """
         Функция отображает траекторию
         :param ax:
         :type ax: matplotlib.axes.Axes
         :return: None
         """
-        self.ax.set_xlim(self.lim_x)
-        self.ax.set_ylim(self.lim_y)
-        self.ax.set_zlim(self.lim_z)
-        self.ax.quiver(self.point[0], self.point[1], self.point[2],
+        ax.set_xlim(self.lim_x)
+        ax.set_ylim(self.lim_y)
+        ax.set_zlim(self.lim_z)
+        ax.quiver(self.point[0], self.point[1], self.point[2],
                   self.orientation[0, 0], self.orientation[0, 1], self.orientation[0, 2],
                       length=self.length, color='r')
-        self.ax.quiver(self.point[0], self.point[1], self.point[2],
+        ax.quiver(self.point[0], self.point[1], self.point[2],
                   self.orientation[1, 0], self.orientation[1, 1], self.orientation[1, 2],
-                      length=self.length, color='b')
-        self.ax.quiver(self.point[0], self.point[1], self.point[2],
+                      length=self.length, color='g')
+        ax.quiver(self.point[0], self.point[1], self.point[2],
                   self.orientation[2, 0], self.orientation[2, 1], self.orientation[2, 2],
-                  length=self.length, color='g')
+                  length=self.length, color='b')
+        if self.text:
+            if self.name_body != None:
+                ax.text(self.point[0], self.point[1], self.point[2],
+                        f"{self.name_body}: \n {self.point[0], self.point[1], self.point[2]}", color='blue')
+            else:
+                ax.text(self.point[0], self.point[1], self.point[2],
+                    f"{np.round(self.point[0], 3), np.round(self.point[1], 3), np.round(self.point[2], 3)}", color='blue')
