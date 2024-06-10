17576a7254a6ca84470dab3e0352e5cb4e8c93e8
Michael <onisoris@yandex.ru>
2024-06-10 17:40:12 +0300

    Тестовый файл с поворотами массива дронов

--- tests/test_Drone.py
+++ tests/test_Drone.py
@@ -1,21 +1,9 @@
 from swarmsys import *
 from dspl import Dspl
 import numpy as np
-from body import Body
 from ThreeDTool import Points
-
 point = np.array([0, 1, 0])
 ori = np.eye(3)
-
-# drone = Drone(point, ori)
-# drone2 = Drone(point, ori)
-# drone2.rot_z(np.pi/4, apply=True)
-#
-# drone3 = Drone(point, ori)
-# drone3.rot_z(np.pi/4*2, apply=True)
-#
-# drone4 = Drone(point, ori)
-# drone4.rot_z(np.pi/4*3, apply=True)
 rot_point = np.array([1, 0, 0])
 zero_point = np.array([0, 0, 0])
 drones = []
@@ -25,25 +13,8 @@ for i in range(8):
 point_rot = Points([rot_point, zero_point], text=True, s=10)
 drones.append(point_rot)
 dp = Dspl(drones, create_subplot=True, qt=True)
-# drone.rot_x(np.pi/4, apply=True)
-# drone.self_show()
+lim_1 = -2
+lim_2 = 2
+dp.limits(x=[lim_1, lim_2], y=[lim_1, lim_2], z=[lim_1, lim_2])
+
 dp.show()
-#
-# for i in range(4):
-#     drones.append(Drone(point=[i, i, 0], orientation=[(-1)**i, i, 0]))
-#
-#
-# dp.ax.axes.set_xlim3d(left=-5, right=5)
-# dp.ax.axes.set_ylim3d(bottom=-5, top=5)
-# dp.ax.axes.set_zlim3d(bottom=0, top=5)
-# dp.show()
-#
-# for dr in drones:
-#     dr.rot_z(np.pi/3)
-# drone.rot_z(np.pi/4)
-# dp.create_subplot3D()
-# dp.ax.axes.set_xlim3d(left=-5, right=5)
-# dp.ax.axes.set_ylim3d(bottom=-5, top=5)
-# dp.ax.axes.set_zlim3d(bottom=0, top=5)
-#
-# dp.show()
