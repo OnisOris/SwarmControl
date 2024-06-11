28f674ee1e2ed9e1931d75f08faeb79f8f2c44c7
Michael <onisoris@yandex.ru>
2024-06-11 15:31:44 +0300

    Установка дронов и тестировния self_show - надо поправить отображение осе

--- tests/test_create_trajectory.py
+++ tests/test_create_trajectory.py
@@ -1,7 +1,11 @@
 import ThreeDTool
 from swarmsys import *
 
-drone1 = Drone(point=[0, 0, 1])
-drone2 = Drone(point=[1, 0, 1])
+drone1 = Drone(point=[2.5, 2.5, 0])
+drone2 = Drone(point=[4.5, 6.5, 0])
 
+plan_point = [8, 11, 0]
 
+darr = Darray(drones=[drone1, drone2])
+
+darr.self_show()
