c3897ebc801a2606cd74443e6a65ebdb78be8d24
Michael <onisoris@yandex.ru>
2024-06-14 12:51:53 +0300

    Отдельное вращение одного массива с дронами

--- tests/test_func/test_Darray_rot_z.py
+++ tests/test_func/test_Darray_rot_z.py
@@ -19,3 +19,17 @@ dp.ax.title.set_text(f'Вращение массива дронов по оси 
 dp.show()
 
 
+rot_point = np.array([40, 0, 0])
+zero_point = np.array([0, 0, 0])
+p = Points([rot_point, zero_point], text=True)
+darr = Darray()
+darr.create_square_array(center_point=np.array([0, 0, 0]))
+darr.rot_z(np.pi/4, rot_point=rot_point, apply=True)
+
+dp = Dspl([darr] + [p], qt=True)
+lim_1 = -20
+lim_2 = 30
+dp.limits(x=[lim_1, lim_2], y=[lim_1, lim_2], z=[lim_1, lim_2])
+dp.ax.title.set_text(f'Вращение массива дронов по оси z и точки {rot_point}')
+dp.show()
+
