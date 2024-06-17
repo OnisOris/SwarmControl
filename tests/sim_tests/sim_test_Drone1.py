e9267061a0e100ffa7a8ce68944fe67c7905a938
Michael <onisoris@yandex.ru>
2024-06-17 15:57:43 +0300

    более удобное добавление портов

--- tests/sim_tests/sim_test_Drone1.py
+++ tests/sim_tests/sim_test_Drone1.py
@@ -12,10 +12,10 @@ class IpPort:
 
 
 class DroneConnectingData:
-    drone0: IpPort = IpPort(ip="127.0.0.1", port=8000)
-    drone1: IpPort = IpPort(ip="127.0.0.1", port=8001)
-    drone2: IpPort = IpPort(ip="127.0.0.1", port=8002)
-    drone3: IpPort = IpPort(ip="127.0.0.1", port=8003)
+    drone0: IpPort = IpPort(ip="127.0.0.1", port=8003)
+    drone1: IpPort = IpPort(ip="127.0.0.1", port=8007)
+    drone2: IpPort = IpPort(ip="127.0.0.1", port=8005)
+    drone3: IpPort = IpPort(ip="127.0.0.1", port=8004)
 
 
 pioner = Pioneer(ip=DroneConnectingData.drone0.ip, mavlink_port=DroneConnectingData.drone0.port)
@@ -23,8 +23,6 @@ dr = Drone(drone=pioner, apply=True)
 dr.arm()
 dr.takeoff()
 dr.goto([1, -2, 1], apply=True)
-# a = np.allclose(np.array(pioner.get_local_position_lps()), np.array([1, -2, 1]), 1e-4)
-# print(a)
 n = False
 from loguru import logger
 while not n:
@@ -32,13 +30,7 @@ while not n:
     if isinstance(k, list):
         logger.debug(k)
         n = np.allclose(np.array(k), np.array([1, -2, 1]), 1e-1)
-    # if a is not None:
-    #     print(a.__class__)
-# # time.sleep(7)
-# while not pioner.point_reached():
-#     a = pioner.get_local_position_lps()
-#     if a is not None:
-#         print(a)
+
 for i in range(8):
     dr.rot_z(np.pi / 4, apply=True)
     n = False
@@ -47,13 +39,3 @@ for i in range(8):
         if isinstance(k, list):
             logger.debug(k)
             n = np.allclose(np.array(k), dr.body.point, 1e-1)
-
-# # time.sleep(3)
-# while not pioner.point_reached():
-#     a = pioner.get_local_position_lps()
-#     if a is not None:
-#         print(a)
-# while not pioner.point_reached():
-#     a = pioner.get_local_position_lps()
-#     if a is not None:
-#         print(a)
