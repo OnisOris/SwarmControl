d0c5d1d788215316e08740b6cc5b254b87fb259f
Michael <onisoris@yandex.ru>
2024-06-18 17:08:03 +0300

    Пример расчета пересечений с дронами

---
+++ tests/visual_tests/Map.py
@@ -0,0 +1,34 @@
+from scheduler import *
+from swarmsys import *
+from ThreeDTool import Dspl, Line_segment, Points
+import matplotlib
+matplotlib.use('WebAgg')
+
+darr = Darray()
+darr.create_square_array(sizes=np.array([[-1, 1],
+                                         [-1, 1]]),
+                         number_of_drones=2)
+
+m = Map([darr])
+
+m.grab_borders()
+
+ls = np.array([])
+points = []
+for i, drone in enumerate(darr):
+    if i != np.shape(darr.drones)[0]-1:
+        ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[i+1].body.point)))
+        points.append(drone.get_polygon().point_of_intersection(darr[i+1].body.point))
+    else:
+        ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[0].body.point)))
+        points.append(drone.get_polygon().point_of_intersection(darr[0].body.point))
+
+
+pp = []
+for p in points:
+    pp.append(Points(p, s=15, color='red'))
+
+dp = Dspl(np.hstack([m.borders, ls, pp]))
+# dp = Dspl(pp)
+dp.show()
+
