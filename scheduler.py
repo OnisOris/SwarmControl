c4dbfcdb0bf265030ab3c5583eb9aa7e7d196ca6
Michael <onisoris@yandex.ru>
2024-06-18 17:07:35 +0300

    Описание класса

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+
+def tr_cre():
+    pass
+
+
 class Map:
     def __init__(self, objects: list | np.ndarray):
         self.objects = objects
@@ -7,11 +12,13 @@ class Map:
 
     def grab_borders(self):
         for obj in self.objects:
-            bord = obj.get_border()
+            bord = obj.get_polygon()
             self.borders = np.hstack([self.borders, bord])
 
 
-
 class Sheduler:
+    """
+    Данный класс принимает в себя ссылку на карту с объектами классов: Drone, Darray.
+    """
     def __init__(self, map_object: Map):
         pass
