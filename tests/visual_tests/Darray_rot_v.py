117e76af525f1cb68806c634392d654360039a93
Michael <onisoris@yandex.ru>
2024-06-24 16:21:27 +0300

    Убраны комментарии

--- tests/visual_tests/Darray_rot_v.py
+++ tests/visual_tests/Darray_rot_v.py
@@ -1,7 +1,6 @@
 from swarmsys import *
 from ThreeDTool import Points
 import matplotlib
-import matplotlib.pyplot as plt
 matplotlib.use('WebAgg')
 
 rot_point = np.array([40, 0, 0])
@@ -21,6 +20,5 @@ lim_2 = 30
 dp.limits(x=[lim_1, lim_2], y=[lim_1, lim_2], z=[lim_1, lim_2])
 dp.ax.quiver(*rot_point, *axis,
                       length=20, color='orange')
-# dp.ax.title.set_text(f'Вращение дрона по оси z и точки {rot_point}')
 dp.ax.title.set_text(f'Вращение массива дронов по оси z и точки {rot_point}')
 dp.show()
