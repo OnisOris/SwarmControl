8ec5edcdcce9133c43e3d60703af3840bda7d30d
Michael <onisoris@yandex.ru>
2024-06-27 10:56:14 +0300

    Тестовый файл с расчетом пути

--- tests/visual_tests/Map.py
+++ tests/visual_tests/Map.py
@@ -11,24 +11,26 @@ darr.create_square_array(sizes=np.array([[-1, 1],
 
 m = Map([darr])
 
-m.grab_borders()
+# m.grab_borders()
 
-ls = np.array([])
-points = []
-for i, drone in enumerate(darr):
-    if i != np.shape(darr.drones)[0]-1:
-        ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[i+1].body.point)))
-        points.append(drone.get_polygon().point_of_intersection(darr[i+1].body.point))
-    else:
-        ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[0].body.point)))
-        points.append(drone.get_polygon().point_of_intersection(darr[0].body.point))
+darr[0].calculate_path([1, -1, 1], m)
+
+# ls = np.array([])
+# points = []
+# for i, drone in enumerate(darr):
+#     if i != np.shape(darr.drones)[0]-1:
+#         ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[i+1].body.point)))
+#         points.append(drone.get_polygon().point_of_intersection(darr[i+1].body.point))
+#     else:
+#         ls = np.hstack((ls, Line_segment(point1=drone.body.point, point2=darr[0].body.point)))
+#         points.append(drone.get_polygon().point_of_intersection(darr[0].body.point))
 
 
-pp = []
-for p in points:
-    pp.append(Points(p, s=15, color='red'))
+# pp = []
+# for p in points:
+#     pp.append(Points(p, s=15, color='red'))
 
-dp = Dspl(np.hstack([m.borders, ls, pp]))
+# dp = Dspl(np.hstack([m.borders, ls, pp]))
 # dp = Dspl(pp)
-dp.show()
+# dp.show()
 
