d08398a32fd767ba2a94dc2e052a847183acfad8
Mihail Solovev <m.solovev@geoscan.ru>
2024-08-10 14:15:50 +0300

    Добавлен конфиг с поворотами получаемых координат и отправляемых, а также с Ip, портами, периодом отправки скоростей

---
+++ config.py
@@ -0,0 +1,12 @@
+from numpy import pi
+CONFIG = {
+    'standard_port': 5656,
+    'trajectory_write': True,
+    'ip_3': '10.1.100.',
+    'period_send_v': 0.05,
+    'height': 0.12,
+    'length': 0.29,
+    'width':  0.29,
+    'rot_get_pos': 0,
+    'rot_send_U': - pi/2
+}
