f8ed6ba987845d6b40e167c6518ffa7020904d81
Mihail Solovev <m.solovev@geoscan.ru>
2024-08-10 20:30:42 +0300

    Скорректированный файл конфига

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ CONFIG = {
     'standard_port': 5656,
     'trajectory_write': True,
     'ip_3': '10.1.100.',
+    'num_drone': 105,
     'period_send_v': 0.05,
     'height': 0.12,
-    'length': 0.29,
+    'lenght': 0.29,
     'width':  0.29,
     'rot_get_pos': 0,
-    'rot_send_U': - pi/2
+    'rot_send_U': -pi/2
 }
