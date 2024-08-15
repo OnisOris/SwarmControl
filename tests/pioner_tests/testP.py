283de776438250d52fd75d1ced7f2cb35b276282
Mihail Solovev <m.solovev@geoscan.ru>
2024-08-15 08:27:14 +0300

    Более адекватная проверка работы локуса

--- tests/pioner_tests/testP.py
+++ tests/pioner_tests/testP.py
@@ -1,25 +1,26 @@
 from SwarmControl.swarmsys import *
 from pioneer_sdk import Pioneer
 import time
+from config import CONFIG
 
 # import icecream as ic
 
 pioneer = Pioneer(ip="10.1.100.108", mavlink_port=5656, logger=False)
-drone = Drone(drone=pioneer, apply=True)
+drone = Drone(CONFIG, drone=pioneer, apply=True)
 drone.arm()
-time.sleep(4)
+time.sleep(6)
 drone.takeoff(1.8)
 
 
 drone.set_coord_check()
 time.sleep(3)
-coord = [-2, -2, 1.5]
+coord = [-4, -4, 1.5]
 # coord = rot_z(coord, -pi / 2)
-drone.goto(coord, apply=True)
-drone.wait_for_point(coord, accuracy=1e-1)
-drone.set_v()
+drone.goto(coord, apply=True, wait_point=False)
+# drone.wait_for_point(coord, accuracy=1e-1)
+# drone.set_v()
 drone.xyz_flag = True
-# time.sleep(10)
+time.sleep(12)
 # start_pos = np.array(dr.get_local_position_lps())
 
 # time.sleep(5)
@@ -30,11 +31,20 @@ t0 = time.time()
 k = 0
 # drone.write_traj = True
 # print("fff")
-while k < np.pi/2*6:
-    # k += np.pi/2
-    drone.speed_change([sin(k), cos(k)])
-    # v.speed = [sin(k), cos(k)]
-    # print(f"xyz = {v.xyz} \n speed = {v.speed}")
-    time.sleep(5)
-    k += np.pi / 2
+drone.goto([-4, 4, 1.5], apply=True,  wait_point=False)
+time.sleep(12)
+drone.goto([4, 4, 1.5], apply=True, wait_point=False)
+time.sleep(12)
+drone.goto([4, -4, 1.5], apply=True, wait_point=False)
+time.sleep(12)
+drone.goto([-4, -4, 1.5], apply=True, wait_point=False)
+time.sleep(12)
+# while k < np.pi/2*4:
+#     # k += np.pi/2
+#     drone.speed_change([sin(k), cos(k)])
+#     # v.speed = [sin(k), cos(k)]
+#     # print(f"xyz = {v.xyz} \n speed = {v.speed}")
+#     time.sleep(7)
+#     k += np.pi / 2
 
+drone.save_data('state_count.csv')
